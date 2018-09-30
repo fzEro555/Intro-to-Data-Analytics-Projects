7665405d1ac8e8c4ea90b7595db951a3d328409b
peuleupeu <mh1795@georgetown.edu>
2018-09-30 15:22:59 -0400

    change starting year to 1920 because electionsbycounty has data only date back to then

--- nytimes/archieve_api.py
+++ nytimes/archieve_api.py
@@ -6,7 +6,7 @@ import time
 if __name__ == "__main__":
     base_url = "https://api.nytimes.com/svc/archive/v1"
     api_key = "e7bbf035e3694a01894a2b523c4c589e"
-    for year in range(1852, 2018):
+    for year in range(1920, 2018):
         for month in range(1, 13):
             url = "{}/{}/{}.json?api-key={}".format(base_url, year, month, api_key)
             time.sleep(60)
