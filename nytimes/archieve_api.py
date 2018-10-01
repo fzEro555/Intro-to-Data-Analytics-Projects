a5f565505f1fd7e0df2cfc130d5ac74f2a0808c8
peuleupeu <mh1795@georgetown.edu>
2018-10-01 09:42:38 -0400

    modify to handle error

--- nytimes/archieve_api.py
+++ nytimes/archieve_api.py
@@ -3,15 +3,35 @@ import json
 import time
 
 
+def retrieve(base_url, api_key):
+    for year in range(2018, 2019):
+        for month in range(8, 13):
+            url = "{}/{}/{}.json?api-key={}".format(base_url, year, month, api_key)
+            print("\nrequesting for {}/{}".format(month, year))
+            response = requests.get(url)
+            print("status code: {}".format(response.status_code))
+            if response.status_code is 200:
+                with open("{}_{}.json".format(year, month), 'w') as file:
+                    file.write(json.dumps(response.json(), indent=4))
+            else:
+                with open("{}_{}_{}.txt".format(year, month, response.status_code), 'w') as file:
+                    file.writelines(response.text)
+
+
+def test_for_error(base_url, api_key):
+    year = 1851
+    month = 1
+    url = "{}/{}/{}.json?api-key={}".format(base_url, year, month, api_key)
+    response = requests.get(url)
+    if response.status_code is 200:
+        print(json.dumps(response.json(), indent=4))
+    else:
+        print(response.text)
+
+
 if __name__ == "__main__":
     base_url = "https://api.nytimes.com/svc/archive/v1"
     api_key = "e7bbf035e3694a01894a2b523c4c589e"
-    for year in range(1920, 2018):
-        for month in range(1, 13):
-            url = "{}/{}/{}.json?api-key={}".format(base_url, year, month, api_key)
-            time.sleep(60)
-            response = requests.get(url)
-            with open("{}_{}.txt".format(year, month), 'w') as file:
-                file.write(json.dumps(response.json()))
-
-    input("Press anything to exit. ")
+    retrieve(base_url, api_key)
+    # test_for_error(base_url, api_key)
+    # input("Press anything to exit. ")
