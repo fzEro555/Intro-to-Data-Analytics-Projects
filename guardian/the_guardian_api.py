a4aa0b300fe5517848ce94a96b21a7c96cec4797
arempe <anr49@georgetown.edu>
2018-10-03 16:10:23 -0400

    modified code to pull from dates of each hurricane

--- guardian/the_guardian_api.py
+++ guardian/the_guardian_api.py
@@ -2,22 +2,24 @@ import requests
 import json
 
 
-def get_parameters(api_key):
+def get_parameters(api_key, from_date, to_date):
     parameters = {"api-key": api_key,
                   "format": "json",
                   "lang": "en",
-                  "from-date": "2017-5-1",
-                  "to-date": "2017-12-31",
+                  "from-date": from_date,
+                  "to-date": to_date,
                   "show-fields": ["all"]}
     return parameters
 
 
-def retrieve(base_url, api_key):
+def retrieve(base_url, api_key, hurricane_name, from_date, to_date):
     print("\nrequesting")
-    response = requests.get(base_url, get_parameters(api_key))
+    response = requests.get(base_url, get_parameters(api_key, from_date, to_date))
+    json_response = response.json()
+    print(len(json_response["response"]["results"]))
     print("status code: {}".format(response.status_code))
     if response.status_code is 200:
-        with open("the_guardian_response.json", 'w') as file:
+        with open("the_guardian_response_{}.json".format(hurricane_name), 'w') as file:
             file.writelines(json.dumps(response.json(), indent=4))
     else:
         with open("the_guardian_response_{}.json".format(response.status_code), 'w') as file:
@@ -25,7 +27,21 @@ def retrieve(base_url, api_key):
 
 
 if __name__ == "__main__":
+    
+    irma_from = "2017-8-30"
+    irma_to = "2017-9-13"
+    
+    harvey_from = "2017-8-17"
+    harvey_to = "2017-9-2"
+    
+    maria_from = "2017-9-16" 
+    maria_to = "2017-10-2"
+    
     api_key = "ede93040-cd7a-4350-a2f7-6b0426126659"
     base_url = "https://content.guardianapis.com/search"
-    retrieve(base_url, api_key)
+    
+    retrieve(base_url, api_key, "irma", irma_from, irma_to)
+    retrieve(base_url, api_key, "harvey", harvey_from, harvey_to)
+    retrieve(base_url, api_key, "maria", maria_from, maria_to)
+    
     # input("Press key to exit. ")
