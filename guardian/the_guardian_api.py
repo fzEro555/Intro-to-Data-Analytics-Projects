e9cde5b5b74806901d2d7532ef6b575c5c6fe351
peuleupeu <mh1795@georgetown.edu>
2018-10-05 11:02:11 -0400

    fix the guardian api

--- guardian/the_guardian_api.py
+++ guardian/the_guardian_api.py
@@ -2,46 +2,61 @@ import requests
 import json
 
 
-def get_parameters(api_key, from_date, to_date):
+def get_parameters(api_key, from_date, to_date, page):
     parameters = {"api-key": api_key,
                   "format": "json",
                   "lang": "en",
                   "from-date": from_date,
                   "to-date": to_date,
+                  "page": page,
+                  "page-size": "50",
                   "show-fields": ["all"]}
     return parameters
 
 
-def retrieve(base_url, api_key, hurricane_name, from_date, to_date):
-    print("\nrequesting")
-    response = requests.get(base_url, get_parameters(api_key, from_date, to_date))
-    json_response = response.json()
-    print(len(json_response["response"]["results"]))
+def write_response(response, page: str, hurricane_name: str):
     print("status code: {}".format(response.status_code))
     if response.status_code is 200:
-        with open("the_guardian_response_{}.json".format(hurricane_name), 'w') as file:
+        with open("the_guardian_response_{}_{}.json".format(hurricane_name, page), 'w') as file:
             file.writelines(json.dumps(response.json(), indent=4))
     else:
-        with open("the_guardian_response_{}.json".format(response.status_code), 'w') as file:
+        with open("the_guardian_response_{}_{}_{}.json".format(
+                hurricane_name, page, response.status_code), 'w') as file:
             file.writelines(json.dumps(response.txt))
+
+
+def retrieve(base_url, api_key, hurricane_name, from_date, to_date):
+    print("\ninitial request")
+    response = requests.get(base_url, get_parameters(api_key, from_date, to_date, "1"))
+    write_response(response, "1", hurricane_name)
+    n_pages = response.json()["response"]["pages"]
+    for n in range(2, n_pages+1):
+        print("\nrequesting page {}/{}".format(n, int(n_pages)))
+        response = requests.get(base_url, get_parameters(api_key, from_date, to_date, str(n)))
+        write_response(response, str(n), hurricane_name)
 
 
 if __name__ == "__main__":
     
-    irma_from = "2017-8-30"
-    irma_to = "2017-9-13"
-    
-    harvey_from = "2017-8-17"
-    harvey_to = "2017-9-2"
-    
-    maria_from = "2017-9-16" 
-    maria_to = "2017-10-2"
+    # irma_from = "2017-8-30"
+    # irma_to = "2017-9-13"
+    #
+    # harvey_from = "2017-8-17"
+    # harvey_to = "2017-9-2"
+    #
+    # maria_from = "2017-9-16"
+    # maria_to = "2017-10-2"
+
+    all_from = "2017-6-1"
+    all_to = "2017-12-31"
     
     api_key = "ede93040-cd7a-4350-a2f7-6b0426126659"
     base_url = "https://content.guardianapis.com/search"
     
-    retrieve(base_url, api_key, "irma", irma_from, irma_to)
-    retrieve(base_url, api_key, "harvey", harvey_from, harvey_to)
-    retrieve(base_url, api_key, "maria", maria_from, maria_to)
+    # retrieve(base_url, api_key, "irma", irma_from, irma_to)
+    # retrieve(base_url, api_key, "harvey", harvey_from, harvey_to)
+    # retrieve(base_url, api_key, "maria", maria_from, maria_to)
+
+    retrieve(base_url, api_key, "all", all_from, all_to)
     
     # input("Press key to exit. ")
