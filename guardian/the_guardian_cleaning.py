61817bdcb0cce96ef7ec394c3af3f43e6b3821ca
peuleupeu <mh1795@georgetown.edu>
2018-10-26 16:31:11 -0400

    comment code and add header to csv files

--- guardian/the_guardian_cleaning.py
+++ guardian/the_guardian_cleaning.py
@@ -1,10 +1,10 @@
 import os
-import glob
 import re
 import csv
 import json
 
 
+# count occurrence of a word using regular expression
 def count(text, regular_expression) -> int:
     try:
         _all = regular_expression.findall(text)
@@ -14,11 +14,18 @@ def count(text, regular_expression) -> int:
         return 0
 
 
+# extract year-month-date from the date in the json files
+def process_date(iso_date):
+    full_date = iso_date.split('T', 1)[0]
+    return full_date.strip()
+
+
+# extract counts from the json_file using regular expressions
 def extract_from(json_file, r_hurricane, r_irma, r_harvey, r_maria):
     lst = []
     # get all docs from the json file
     results = json_file["response"]["results"]
-    # for each doc, make a list of id, date, n_hurricane, n_irma, n_harvey, n_maria
+    # for each doc, make a list of date, n_hurricane, n_irma, n_harvey, n_maria
     for res in results:
         fields = res["fields"]
         date = None
@@ -28,30 +35,38 @@ def extract_from(json_file, r_hurricane, r_irma, r_harvey, r_maria):
             date = fields["newspaperEditionDate"]
         headline = fields["headline"]
         bodytext = fields["bodyText"]
-        # count
+        # count occurrence of each word in headline and bodytext
         n_hurricane = count(headline, r_hurricane) + count(bodytext, r_hurricane)
         n_irma = count(headline, r_irma) + count(bodytext, r_irma)
         n_harvey = count(headline, r_harvey) + count(bodytext, r_harvey)
         n_maria = count(headline, r_maria) + count(bodytext, r_maria)
-        lst.append([date, n_hurricane, n_irma, n_harvey, n_maria])
+        lst.append([process_date(date), n_hurricane, n_irma, n_harvey, n_maria])
     return lst
 
 
+# the main method
 if __name__ == "__main__":
+    # compile regular expression for words: hurricane, irma, harvey, maria
+    # they will be later used for counting the number of occurrence of each word in each article
     r_hurricane = re.compile('hurricane', re.IGNORECASE)
     r_irma = re.compile('irma', re.IGNORECASE)
     r_harvey = re.compile('harvey', re.IGNORECASE)
     r_maria = re.compile('maria', re.IGNORECASE)
-    # a list of id, date, n_hurricane, n_irma, n_harvey, n_maria for each news from nytimes
-    ll = []
-    # list all json files in the directory
+
+    # create a list of date, n_hurricane, n_irma, n_harvey, n_maria for each news from the guardian
+    ll = [["news date", "occurrence of hurricane", "occurrence of irma", "occurrence of harvey", "occurrence of maria"]]
+    # list all files in the directory
     path = "./response_archive/"
     json_files = os.listdir(path)
     for jf in json_files:
-        # print("\nCleaning {}".format(jf))
-        with open(path + jf, 'r') as file:
-            json_file = json.loads(file.read())
-            ll.extend(extract_from(json_file, r_hurricane, r_irma, r_harvey, r_maria))
+        # if it is a json file, extract number of occurrence of words and append them to the list
+        if jf.endswith(".json"):
+            # print("\nCleaning {}".format(jf))
+            with open(path + jf, 'r') as file:
+                json_file = json.loads(file.read())
+                ll.extend(extract_from(json_file, r_hurricane, r_irma, r_harvey, r_maria))
+
+    # save the list to a csv file
     print("\nSaving as csv. ")
     with open("the_guardian_cleaned.csv", 'w') as file:
         writer = csv.writer(file, delimiter=',')
--- news_further_cleaning/news_extract.py
+++ news_further_cleaning/news_extract.py
@@ -1,6 +1,7 @@
 import csv
 
 
+# check if a line is not all zeros
 def not_all_zeros(ll):
     for e in ll:
         if e is not "0":
@@ -8,18 +9,24 @@ def not_all_zeros(ll):
     return False
 
 
+# clean the file by extracting those articles that have counts that are not all zeros
+# meaning that only keep the articles that mentioned ay of the four words: hurricane, irma, harvey, maria
 def extract(read_file, start, end, name):
+    # read csv file
     reader = csv.reader(read_file, delimiter=',')
     lst = list(reader)
+    # keep only lines have counts that are not all zeros
     lst_lean = [line for line in lst if not_all_zeros(line[start:end])]
+    # save extracted lines to a csv file
     with open(name, 'w') as write_file:
         writer = csv.writer(write_file, delimiter=',')
         writer.writerows(lst_lean)
 
 
+# the main method
 if __name__ == "__main__":
     with open("../nytimes/nytimes_cleaned.csv", 'r') as file:
-        extract(file, 2, 6, "nytimes_lean.csv")
-    with open("../guardian/the_guardian_cleaned.csv") as file:
+        extract(file, 1, 5, "nytimes_lean.csv")
+    with open("../guardian/the_guardian_cleaned.csv", 'r') as file:
         extract(file, 1, 5, "the_guardian_lean.csv")
     # input("press any key to exit")
