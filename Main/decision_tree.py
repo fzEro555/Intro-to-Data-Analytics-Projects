efba041ba71dae447ac4d77be0af100f7dc56c15
arempe <anr49@georgetown.edu>
2018-11-01 19:45:58 -0400

    decision tree changes

--- Main/decision_tree.py
+++ Main/decision_tree.py
@@ -12,28 +12,31 @@ from sklearn.model_selection import KFold
 from sklearn import preprocessing
 import numpy as np
 from sklearn.preprocessing import Normalizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
 
 
 def upload_data(csv_file: str) -> pd.core.frame.DataFrame:
 	# grab data from csv file provided
-	data_frame = pd.read_csv(csv_file, sep=',', encoding='latin1')
+	# ************ final data wrong?
+	data_frame = pd.read_csv(csv_file, sep=',', skiprows=363)
 
-	print(data_frame.head())
+	# print(data_frame.head(364))
 
 	# cleaning
 
-	data_frame['year'], data_frame['month'], data_frame['day'] = zip(
-		*data_frame['news date'].apply(lambda x: x.split('-')))
-
-	del data_frame['news date']
+	# data_frame['year'], data_frame['month'], data_frame['day'] = zip(
+	# 	*data_frame['date'].apply(lambda x: x.split('-')))
+	#
+	# del data_frame['date']
 
 	print(data_frame.head())
 
-	# normalize data
-	normalized = Normalizer().fit_transform(data_frame['day'])
-
-	print(normalized, type(normalized))
-	print(data_frame.head())
+	# # normalize data
+	# normalized = Normalizer().fit_transform(data_frame['day'])
+	#
+	# print(normalized, type(normalized))
+	# print(data_frame.head())
 
 	# print(norm_data)
 
@@ -48,11 +51,16 @@ def upload_data(csv_file: str) -> pd.core.frame.DataFrame:
 # separate test/train data
 def separate_data(data_frame: pd.core.frame.DataFrame) -> tuple:
 	val_array = data_frame.values
-	X = val_array[:, 0:4]
-	Y = val_array[:, 4]
+	X = val_array[:, 1:5]
+	Y = val_array[:, 5]
+
+	print(X)
+	print(Y)
 	test_size = 0.20
 	seed = 7
-	return train_test_split(X, Y, test_size=test_size, random_state=seed)
+	X_train, X_validate, Y_train, Y_validate = train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+	return X_train, X_validate, Y_train, Y_validate
 
 
 def run_decision_tree(split_data: tuple):
@@ -61,6 +69,11 @@ def run_decision_tree(split_data: tuple):
 	# Test options and evaluation metric
 
 	X_train, X_validate, Y_train, Y_validate = split_data
+	print(X_train)
+	print(X_validate)
+	print(Y_train)
+	print(Y_validate)
+
 	num_folds = 10
 	num_instances = len(X_train)
 	seed = 7
@@ -68,19 +81,26 @@ def run_decision_tree(split_data: tuple):
 
 	decision_tree_model = DecisionTreeClassifier()
 
-	results = []
-	names = []
-
 	kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=False)
-	cv_results = cross_val_score(decision_tree_model, X_train, Y_train, cv=kfold, scoring=scoring)
-	results.append(cv_results)
-	msg = "CART decision tree classifier: %f (%f)" % (cv_results.mean(), cv_results.std())
-	print(msg)
+	# cv_results = cross_val_score(decision_tree_model, X_train, Y_train, cv=kfold, scoring=scoring)
+	# msg = "CART decision tree classifier: %f (%f)" % (cv_results.mean(), cv_results.std())
+	# print(msg)
+
+
+def run_random_forest_tree(split_data: tuple):
+	X_train, X_validate, Y_train, Y_validate = split_data
+
+	clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+	clf.fit(X_train, Y_train)
+	print(clf.predict(X_validate))
+
 
 
 if __name__ == "__main__":
-	nytimes_clean = "../nytimes/nytimes_cleaned.csv"
+	combined_final = "combined_data.csv"
 
-	data_frame = upload_data(nytimes_clean)
+	data_frame = upload_data(combined_final)
 
-	run_decision_tree(separate_data(data_frame))
+	# run_decision_tree(separate_data(data_frame))
+
+	run_random_forest_tree(separate_data(data_frame))
