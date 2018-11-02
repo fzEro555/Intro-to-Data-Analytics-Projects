ad86a162fd9092c8e35b2dd8ed45453704a48b8b
peuleupeu <mh1795@georgetown.edu>
2018-11-02 12:07:13 -0400

    modify main a bit

--- Main/main.py
+++ Main/main.py
@@ -10,7 +10,7 @@ from Main.count import count
 
 from Main.combine_lean_data import combine
 
-from hypothesis_testing.decision_tree import main as decision_tree_main
+from hypothesis_testing.decision_tree import main as decision_tree__and_random_forest
 
 
 def get_and_process_data():
@@ -35,7 +35,7 @@ def get_and_process_data():
 
 def hypothesis_testing():
     # hyp1 predict level of hurricane, decision tree, random forest
-    decision_tree_main()
+    decision_tree__and_random_forest()
     # hyp2 storm hits, anova, knn, svm
 
     # hyp3 government spending, naive bayes, linear regression
