db88dd10e293176c84f20870a7a8da5d096bad1d
arempe <anr49@georgetown.edu>
2018-11-02 10:18:33 -0400

    decision tree

--- Main/decision_tree.py
+++ Main/decision_tree.py
@@ -16,33 +16,65 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 
 
+def date_ranges(date: str) -> str:
+	# first two dates from Irene --> category 1
+	# second two Harvey --> category 4
+	# final two Maria --> category 5
+	# all else, no hurricane /
+	date_list = ['2011-08-16', '2011-09-02',
+				 '2017-08-12', '2017-09-08',
+				 '2017-09-11', '2017-10-07']
+
+	category = ""
+	if date < date_list[0]:
+		category = "none"
+	if date_list[0] <= date <= date_list[1]:
+		category = "category 1"
+	if date_list[1] < date < date_list[2]:
+		category = "none"
+	if date_list[2] <= date <= date_list[3]:
+		category = "category 4"
+	if date_list[3] < date < date_list[4]:
+		category = "none"
+	if date_list[4] <= date <= date_list[5]:
+		category = "category 5"
+	if date > date_list[5]:
+		category = "none"
+	return category
+
+
+def category_level(data_frame: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+	for index, row in data_frame.iterrows():
+		data_frame['storm_category'] = data_frame.apply(lambda row: date_ranges(row['date']), axis=1)
+
+
 def upload_data(csv_file: str) -> pd.core.frame.DataFrame:
 	# grab data from csv file provided
 	# ************ final data wrong?
-	data_frame = pd.read_csv(csv_file, sep=',', skiprows=363)
-
-	# print(data_frame.head(364))
+	data_frame = pd.read_csv(csv_file, sep=',')
 
 	# cleaning
 
-	# data_frame['year'], data_frame['month'], data_frame['day'] = zip(
-	# 	*data_frame['date'].apply(lambda x: x.split('-')))
-	#
-	# del data_frame['date']
+	print(data_frame.head())
+
+	df_shape = data_frame.shape
+
+	print(df_shape, df_shape[0], df_shape[1])
 
 	print(data_frame.head())
 
-	# # normalize data
-	# normalized = Normalizer().fit_transform(data_frame['day'])
-	#
-	# print(normalized, type(normalized))
-	# print(data_frame.head())
+	# add categories to data (class will be predicting)
 
-	# print(norm_data)
+	category_level(data_frame)
+	data_frame['storm_category'].fillna("none", inplace=True)
+	print(df_shape, df_shape[0], df_shape[1])
+
+	print(data_frame.head())
+	# data_frame['class'] = np.zeros(df_shape[0])
 
 	# scatter plot of vars
-	scatter_matrix(data_frame)
-	plt.show()
+	# scatter_matrix(data_frame)
+	# plt.show()
 
 	# return normalized df
 	return data_frame
@@ -52,10 +84,19 @@ def upload_data(csv_file: str) -> pd.core.frame.DataFrame:
 def separate_data(data_frame: pd.core.frame.DataFrame) -> tuple:
 	val_array = data_frame.values
 	X = val_array[:, 1:5]
-	Y = val_array[:, 5]
-
-	print(X)
+	Y = val_array[:, 35]
 	print(Y)
+	print(type(data_frame['storm_category'].unique().tolist()))
+	classes = data_frame['storm_category'].unique().tolist()
+	print("classes: {}".format(data_frame['storm_category'].unique().tolist()))
+	print("class counts: {}".format(data_frame['storm_category'].value_counts()))
+	print("x shape: {}".format(X.shape))
+	print("y shape: {}".format(Y.shape))
+
+
+	# Y.astype('str')
+	print(val_array[:, 33], type(val_array[:, 33]), type(Y[0]))
+
 	test_size = 0.20
 	seed = 7
 	X_train, X_validate, Y_train, Y_validate = train_test_split(X, Y, test_size=test_size, random_state=seed)
@@ -69,10 +110,10 @@ def run_decision_tree(split_data: tuple):
 	# Test options and evaluation metric
 
 	X_train, X_validate, Y_train, Y_validate = split_data
-	print(X_train)
-	print(X_validate)
-	print(Y_train)
-	print(Y_validate)
+	# print(X_train)
+	# print(X_validate)
+	# print(Y_train)
+	# print(Y_validate)
 
 	num_folds = 10
 	num_instances = len(X_train)
@@ -82,18 +123,35 @@ def run_decision_tree(split_data: tuple):
 	decision_tree_model = DecisionTreeClassifier()
 
 	kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=False)
-	# cv_results = cross_val_score(decision_tree_model, X_train, Y_train, cv=kfold, scoring=scoring)
-	# msg = "CART decision tree classifier: %f (%f)" % (cv_results.mean(), cv_results.std())
-	# print(msg)
+	cv_results = cross_val_score(decision_tree_model, X_train, Y_train, cv=kfold, scoring=scoring)
+	msg = "CART decision tree classifier: %f (%f)" % (cv_results.mean(), cv_results.std())
+	print(msg)
 
 
 def run_random_forest_tree(split_data: tuple):
 	X_train, X_validate, Y_train, Y_validate = split_data
 
+	num_folds = 10
+	num_instances = len(X_train)
+	seed = 7
+	scoring = 'accuracy'
+
 	clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
 	clf.fit(X_train, Y_train)
-	print(clf.predict(X_validate))
 
+
+	kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=False)
+	cv_results = cross_val_score(clf, X_train, Y_train, cv=kfold, scoring=scoring)
+	msg = "Random forest tree classifier: %f (%f)" % (cv_results.mean(), cv_results.std())
+	print(msg)
+
+
+
+	predictions = clf.predict(X_validate)
+
+	print(accuracy_score(Y_validate, predictions))
+	print(confusion_matrix(Y_validate, predictions))
+	print(classification_report(Y_validate, predictions))
 
 
 if __name__ == "__main__":
