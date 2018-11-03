d741295b20143e657f1431af193bf05dcd565ce1
fzEro5 <736565860@qq.com>
2018-11-02 21:20:19 -0400

    histograms modified

--- Main/histograms.py
+++ Main/histograms.py
@@ -1,4 +1,4 @@
-﻿import csv
+﻿﻿import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -6,8 +6,10 @@ import pandas as pd
 def plot_variables(data):
     # Basic plotting - boxplot of entire dataframe
     plt.style.use = 'default'
+    data = data[['number of titles for irma in reddit', 'number of summaries for irma in reddit',
+                 'number of titles for harvey in reddit', 'number of summaries for harvey in reddit']]
     data.boxplot()
-    plt.title("Box plots of all variables in data set")
+    plt.title("Box plots of 4 variables in data set")
     plt.show()
     # plt.savefig('boxPlot.png')
     # Basic plotting - histogram of entire dataframe
@@ -18,69 +20,52 @@ def plot_variables(data):
     return
 
 
-def plot_correlarion(data):
-    # data = pd.read_csv("counts_combined.csv")
+def plot_correlation(data):
+    data = data[data['hurricane name'].str.contains('Irma|Harvey', na=False)]
+
+    data = data[['number of titles for irma in reddit', 'number of summaries for irma in reddit',
+                 'number of titles for harvey in reddit', 'number of summaries for harvey in reddit']]
+
     # The following will create a collection of subplots
-    # that are histograms for each variable by nytimes count hurricane
-    VariableList = ["nytimes count irma", "nytimes count harvey", "nytimes count maria"]
+    # that are histograms for each variable by age group
+
+    VariableList = ["number of titles for irma in reddit"]
     # print(VariableList)
     for var in VariableList:
-        name = "nytimes_Graph_for_" + var
+        name = "reddit_irma_summaries_Graph_for_" + var
 
-        data[var].hist(by=data['nytimes count hurricane'])
-        plt.suptitle("Histograms by nytimes count hurricane for " + var)
+        data[var].hist(by=data['number of summaries for irma in reddit'])
+        pl.suptitle("Histograms by number of summaries for irma in reddit for " + var)
         plt.savefig(name)
         plt.clf()
         plt.show()
         plt.close()
+        return
+
+
+# scatter plot
+def scatter_plot(data):
+    data = data[data['hurricane name'].str.contains('Irma|Harvey', na=False)]
+
+    data = data[['number of titles for irma in reddit', 'number of summaries for irma in reddit',
+                 'number of titles for harvey in reddit', 'number of summaries for harvey in reddit']]
+
+    plt.scatter(data['number of titles for irma in reddit'], data['number of summaries for irma in reddit'])
+    plt.scatter(data['number of titles for harvey in reddit'], data['number of summaries for harvey in reddit'])
+    # plt.scatter(data['nytimes count maria'], data['nytimes count harvey'])
     return
 
-'''
-# plot histograms
-def plot_histograms(data):
-    plt.figure(0)
-    plt.hist([line[1] for line in data])
-    # add subplot for hurricane in nytimes
-    plt.figure(1)
-    plt.hist([line[5] for line in data])
-    # add subplot for hurricane in guardian
-    plt.figure(2)
-    plt.hist([line[9] for line in data])
-    # show
-    plt.show()
-    return
 
-
-# plot histograms
-def plot_histogram(data):
-    # create a figure
-    fig = plt.figure()
-    # add subplot for hurricane in reddit
-    fig.add_subplot(131)
-    plt.hist([line[1] for line in data])
-    # add subplot for hurricane in nytimes
-    fig.add_subplot(132)
-    plt.hist([line[5] for line in data])
-    # add subplot for hurricane in guardian
-    fig.add_subplot(133)
-    plt.hist([line[9] for line in data])
-    # show
-    plt.show()
-    return
-'''
-
-def main():
+if __name__ == "__main__":
     # with open("./counts_combined.csv", 'r') as combined_file:
     #     reader = csv.reader(combined_file, delimiter=',')
     #     combined = list(reader)
     #
     # plot_histograms(combined[1:])
 
-    data = pd.read_csv("counts_combined.csv")
+    data = pd.read_csv("combined_data.csv")
 
     plot_variables(data)
-    plot_correlarion(data)
-
-
-if __name__ == "__main__":
-    main()
+    scatter_plot(data)
+    plot_correlation(data)
+    # input("any key")
