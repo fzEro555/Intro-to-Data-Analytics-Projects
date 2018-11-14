f8bab3670be3b6d5a96070a5a9becf8cf04a4ef9
arempe <anr49@georgetown.edu>
2018-11-14 17:50:54 -0500

    update sentiment

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -1,8 +1,53 @@
-from nltk.corpus import sentiwordnet as swn
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.classify import SklearnClassifier
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.sentiment.vader import SentiText
 
-print(swn.senti_synset('breakdown.n.03'))
+
+def upload_data(csv_file: str) -> pd.core.frame.DataFrame:
+	# grab data from csv file provided
+	data_frame = pd.read_csv(csv_file, sep=',')
+	print(data_frame.head(5))
+	return data_frame
 
 
-"""
-http://ai.stanford.edu/~amaas/data/sentiment/
-"""
+def get_sent_score(data_frame: pd.core.frame.DataFrame, col_list: list) -> dict:
+	sent_analyzer = SentimentIntensityAnalyzer()
+	score_list = []
+	neg_total = 0
+	pos_total = 0
+	neutral_total = 0
+	total = 0
+	for index, row in data_frame.iterrows():
+		for col in col_list:
+			score = sent_analyzer.polarity_scores(row[col])
+			score_list.append((row[col], score))
+			neg_total += score['neg']
+			pos_total += score['pos']
+			neutral_total += score['neu']
+
+			total += 1
+
+	score_dict = {}
+	score_dict["neg"] = neg_total/total
+	score_dict["pos"] = pos_total/total
+	score_dict["neutral"] = neutral_total/total
+
+	print(score_dict)
+
+	return score_dict
+
+
+def main():
+	# upload reddit data
+	reddit_csv = "../reddit/reddit.csv"
+	data_frame = upload_data(reddit_csv)
+	get_sent_score(data_frame, ["comment"])
+
+
+if __name__ == "__main__":
+	main()
+
+
